5686e6ab25dea2fec46402eb8151b5d943c83a8b
Petros <93052465+pkitazos@users.noreply.github.com>
2025-10-05 14:01:06 +0200

    made env immutable and just clone before lambda-application

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -2,52 +2,49 @@ use std::collections::HashMap;
 
 use crate::{BinOp, Const, Term};
 
-pub fn eval(term: Term, env: HashMap<String, Term>) -> Term {
+pub fn eval(term: Term, env: &HashMap<String, Term>) -> Term {
     match term {
         Term::Const(_c) => term,
 
         Term::Var(v) => env.get(&v).unwrap().to_owned(),
 
-        Term::Pair(m, n) => Term::Pair(
-            Box::new(eval(*m, env.clone())),
-            Box::new(eval(*n, env.clone())),
-        ),
+        Term::Pair(m, n) => Term::Pair(Box::new(eval(*m, env)), Box::new(eval(*n, env))),
 
         Term::Lambda(v, t, m) => Term::Lambda(v, t, m),
 
-        Term::Fst(m) => match eval(*m, env.clone()) {
+        Term::Fst(m) => match eval(*m, env) {
             Term::Pair(a, _b) => *a,
             _ => panic!("runtime error"),
         },
-        Term::Snd(m) => match eval(*m, env.clone()) {
+        Term::Snd(m) => match eval(*m, env) {
             Term::Pair(_a, b) => *b,
             _ => panic!("runtime error"),
         },
 
-        Term::App(m, n) => match eval(*m, env.clone()) {
+        Term::App(m, n) => match eval(*m, env) {
             Term::Lambda(v, _, m_2) => {
                 let mut new_env = env.clone();
-                new_env.insert(v, eval(*n, env.clone()));
-                eval(*m_2, new_env.clone())
+                new_env.insert(v, eval(*n, env));
+                eval(*m_2, &new_env)
             }
             _ => panic!("runtime error"),
         },
 
         Term::Let(v, m, n) => {
             let mut new_env = env.clone();
-            new_env.insert(v, eval(*m, env.clone()));
-            eval(*n, new_env.clone())
+            new_env.insert(v, eval(*m, env));
+            eval(*n, &new_env)
         }
 
-        Term::If(b, m, n) => match eval(*b, env.clone()) {
-            Term::Const(Const::Bool(true)) => eval(*m, env.clone()),
-            Term::Const(Const::Bool(false)) => eval(*n, env.clone()),
+        Term::If(b, m, n) => match eval(*b, env) {
+            Term::Const(Const::Bool(true)) => eval(*m, env),
+            Term::Const(Const::Bool(false)) => eval(*n, env),
             _ => panic!("runtime error"),
         },
 
         Term::BinOp(BinOp::Eq, m, n) => {
-            let m2 = eval(*m, env.clone());
-            let n2 = eval(*n, env.clone());
+            let m2 = eval(*m, env);
+            let n2 = eval(*n, env);
 
             match (m2, n2) {
                 (Term::Const(Const::Bool(b_m)), Term::Const(Const::Bool(b_n))) => {
@@ -63,8 +60,8 @@ pub fn eval(term: Term, env: HashMap<String, Term>) -> Term {
         }
 
         Term::BinOp(op, m, n) => {
-            let m2 = eval(*m, env.clone());
-            let n2 = eval(*n, env.clone());
+            let m2 = eval(*m, env);
+            let n2 = eval(*n, env);
 
             match (op, m2, n2) {
                 (BinOp::Plus, Term::Const(Const::Int(i_m)), Term::Const(Const::Int(i_n))) => {
